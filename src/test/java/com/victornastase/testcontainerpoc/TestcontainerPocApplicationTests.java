75fbe0f47dcb34978b53f9f8e51f975bcb6b0e87
Victor Nastase <victor.gabriel.nastase@gmail.com>
2024-04-20 11:42:26 +0300

    Initial commit

---
+++ src/test/java/com/victornastase/testcontainerpoc/TestcontainerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victornastase.testcontainerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
